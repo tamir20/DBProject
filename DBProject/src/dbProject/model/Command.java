defec5e2b91dbd85d75942589a5727283004bcbf
omar <omarg@feedvisor.com>
2016-06-04 21:02:19 +0300

    Add simple parsing capability + updated the transaction.dat example file

--- DBProject/src/dbProject/model/Command.java
+++ DBProject/src/dbProject/model/Command.java
@@ -2,5 +2,44 @@ package dbProject.model;
 
 public enum Command {
 
-    ALLOCATE_RECORD, INSERT, DELETE, SEARCH, RANGE_SEARCH;
+    ALLOCATE_RECORD("A",16),
+    INSERT("I", 7),
+    DELETE("D", 7),
+    SEARCH("S", 7),
+    RANGE_SEARCH("R", 13),
+    END_TRANSACTION(";", 0);
+
+    private final String startsWith;
+    private final int paramIndex;
+
+    Command(String startsWith, int paramIndex) {
+        this.startsWith = startsWith;
+        this.paramIndex = paramIndex;
+    }
+
+    public String getStartsWith() {
+        return startsWith;
+    }
+
+    public int getParamIndex() {
+        return paramIndex;
+    }
+
+    public static Command getCommand(String line){
+        Command result = null;
+        for (Command command : Command.values()){
+            if (command.getStartsWith().equals(line.substring(0,command.getStartsWith().length()))){
+                result = command;
+            }
+        }
+        return result;
+    }
+
+    public static String getParameters(Command command, String line) {
+        String result = null;
+        if (!command.equals(END_TRANSACTION)){
+            result = line.substring(command.getParamIndex(), line.length()-1);
+        }
+        return result;
+    }
 }
---
+++ DBProject/src/dbProject/model/CommandTest.java
@@ -0,0 +1,70 @@
+package dbProject.model;
+
+import org.junit.Test;
+
+public class CommandTest {
+    @Test
+    public void getCommand() throws Exception {
+        String test1 = "Allocate_record(2, \"BIU\", \"CSE\")\n";
+        String test2 = "Insert(\"BIU\", rid2)\n";
+        String test3 = "Delete(rid1);\n";
+        String test4 = "Search(3)^rid1\n";
+        String test5 = "Range_search(1,8)\n";
+        String test6 = ";";
+
+        if (Command.ALLOCATE_RECORD.equals(Command.getCommand(test1))){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+
+        if (Command.INSERT.equals(Command.getCommand(test2))){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+
+        if (Command.DELETE.equals(Command.getCommand(test3))){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+
+        if (Command.SEARCH.equals(Command.getCommand(test4))){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+
+        if (Command.RANGE_SEARCH.equals(Command.getCommand(test5))){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+
+        if (Command.END_TRANSACTION.equals(Command.getCommand(test6))){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+    }
+
+    @Test
+    public void getParameters() throws Exception {
+        String test1 = "Allocate_record(2, \"BIU\", \"CSE\")";
+        String test2 = "Insert(\"BIU\", rid2)";
+        String test3 = "Delete(rid1)";
+        String test4 = "Search(3)";
+        String test5 = "Range_search(1,8)";
+        String test6 = ";";
+
+        System.out.println(Command.getParameters(Command.ALLOCATE_RECORD, test1));
+        System.out.println(Command.getParameters(Command.INSERT, test2));
+        System.out.println(Command.getParameters(Command.DELETE, test3));
+        System.out.println(Command.getParameters(Command.SEARCH, test4));
+        System.out.println(Command.getParameters(Command.RANGE_SEARCH, test5));
+        System.out.println(Command.getParameters(Command.END_TRANSACTION, test6));
+
+    }
+
+}
